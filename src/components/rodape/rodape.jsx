26dc269c22a4fc8a8115489bffe8e1ec2288e49a
leocscotti <leoscotti03@gmail.com>
2023-11-24 17:26:12 -0300

    rodape arrumado

---
+++ src/components/rodape/rodape.jsx
@@ -0,0 +1,27 @@
+import "./rodape.scss";
+
+export default function Rodape() {
+    return (
+      <footer>
+        <div className="rodape">
+            <h1>Integrantes</h1>
+          <p>Leonardo Cordeiro Scotti RM550769 1TDSPM/ 
+              Cargo: dev back-end python e front-end
+          </p>
+          <p>Leonardo Ferreira Lizier- RM551509 1TDSR/
+              Cargo: dev database
+          </p>
+          <p>Enzo Ross Gallone- RM551754- 1TDSPM/
+              Cargo: dev de machine learning e front-end
+          </p>
+          <p>Edward de Lima- RM989676- 1TDSPW/
+              Cargo: dev back-end java
+          </p>
+          <p>Igor Ribeiro Bezerra- RM550989- 1TDSPM/
+              Cargo: dev front-end e software designer
+          </p>
+          </div>
+          
+      </footer>
+    )
+  }
