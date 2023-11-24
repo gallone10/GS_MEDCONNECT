cb7340bec302132b40dc68aa0ced4b7f598b89f3
IgorRibeiro7 <igorbez.12@icloud.com>
2023-11-24 14:59:17 -0300

    atualização da página

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,19 +5,11 @@ import style from '../app/page.scss'
 const HomePage = () => {
   return (
     
-    <div>
-      <h1>Relógio IoT para Monitoramento de Sinais Vitais</h1>
-      <p>
-        Bem-vindo à nossa aplicação que apresenta informações sobre um relógio IoT que monitora os sinais vitais de pacientes.
-      </p>
-      <img src= "../assets/relogio.jpg" alt="Relógio IoT"/>
-      <p>
-        Este relógio é usado pelos médicos para monitorar os sinais vitais dos pacientes de forma conveniente.
-      </p>
+    <div className='fundo'>
+
       <Link href="/about">
         Saiba mais sobre o relógio IoT
       </Link>
-      
     </div>
   );
 };
--- src/components/cabecalho/page.jsx
+++ src/components/cabecalho/page.jsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
+import style from './page.scss'
 
 export default function Cabecalho() {
   return (
     <header className='cabecalho'>
-    <nav>
+      <div className="imagem">
+        <Image src="/logo.png"alt= "logo" 
+        width={300} height={100}></Image>
+      </div>
+    <nav className="menu">
       <Link href="/">HOME</Link>
       <Link href="/produtos">PRODUTOS</Link>
       <Link href="/cadastro">CADASTRO</Link>
